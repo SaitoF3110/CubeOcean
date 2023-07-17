b263fe72e794a1114cc38f79ae834fc6ae9e7ed5
SaitoF3110 <water.tokotoko.drive@gmail.com>
2023-07-17 10:21:17 +0900

    スプライト追加

--- Assets/sclipt/PlayerTest.cs
+++ Assets/sclipt/PlayerTest.cs
@@ -6,7 +6,9 @@ using UnityEngine.UIElements;
 public class PlayerTest : MonoBehaviour
 {
     // Start is called before the first frame update
+    AudioSource _audioRot;
     Rigidbody _rb;
+    int turn = 3;
     float r = 0;
     /// <summary>
     /// ÉvÉåÉCÉÑÅ[Ç™å¸Ç´ÇïœÇ¶ÇÈ
@@ -15,24 +17,48 @@ public class PlayerTest : MonoBehaviour
     public float _rotLimit = 1;
     void Start()
     {
+        _audioRot = GetComponent<AudioSource>();
         _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !_rotMode)
         {
             //transform.position += 5 * transform.up * Time.deltaTime;
             _rb.AddForce(new Vector3(0,300,0));
         }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if(!_rotMode)
+            {
+                _audioRot.Play();
+            }
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                turn = 3;
+            }
+            else
+            {
+                turn = -3;
+            }
+            _rotMode = true;
+            r += turn;
+        }
+
+
+
+    }
+    void FixedUpdate()
+    {
         Vector3 velocity = transform.localPosition;
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && !_rotMode)
         {
             //velocity.x += 0.1f;
             this.transform.Translate(0.1f, 0, 0, Space.Self);
         }
-        else if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A) && !_rotMode)
         {
             //velocity.x += -0.1f;
             this.transform.Translate(-0.1f, 0, 0, Space.Self);
@@ -41,24 +67,11 @@ public class PlayerTest : MonoBehaviour
         {
             //velocity.x += 0;
         }
-        //this.transform.localPosition = velocity;
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            _rotMode = true;
-            r -= 3;
-        }
-
-
-
-    }
-    void FixedUpdate()
-    {
         if (_rotMode)
         {
-            
-            if(r % 90 != 0)
+            if (r % 90 != 0)
             {
-                r -= 3;
+                r += turn;
             }
             else
             {
