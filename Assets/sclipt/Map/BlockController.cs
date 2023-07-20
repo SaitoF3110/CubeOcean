7362867a6f5c945be700688bcd24bd5cd3a224be
SaitoF3110 <water.tokotoko.drive@gmail.com>
2023-07-21 08:32:48 +0900

    alpha値ぃぃぃ

--- Assets/sclipt/Map/BlockController.cs
+++ Assets/sclipt/Map/BlockController.cs
@@ -6,18 +6,14 @@ public abstract class BlockController : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] ObjectType _type = ObjectType.Block;
-    [SerializeField] Material[] _material;
     Vector3 _position;
     [SerializeField] float _fadeSpeed = 0;
+    [SerializeField] Texture _defaultAlbedo;
     public abstract void WhenRotate();
 
     // Update is called once per frame
     private void Start()
     {
-        Vector3 worldPoint = transform.TransformPoint(transform.position);
-        _position.x = worldPoint.x;
-        _position.y = worldPoint.y;
-        _position.z = worldPoint.z;
 
     }
     void Update()
@@ -25,30 +21,11 @@ public abstract class BlockController : MonoBehaviour
         Debug.Log(this.GetComponent<Renderer>().material.color.a);
         if (Input.GetKeyDown(KeyCode.R))
         {
- 
-            if(_type == ObjectType.Block)
-            {
-                if(this.transform.position.x == GameController._intPlayer.x || this.transform.position.z == GameController._intPlayer.x)
-                {
-
-                }
-                else
-                {
-                    //this.GetComponent<Renderer>().material = _material[1];
-                    //GetComponent<Renderer>().material.color = new Color(0, 0, 0, 0.5f);
-                    //this.GetComponent<Renderer>().material.color += new Color(0, 0, 0, 0.5f);
-                        BlockMaterialChange(ChangeType.FadeOut);
-
-                }
-                
-            }
-            else if (_type == ObjectType.Sprite)
-            {
-
-            }
+            BlockControll();
+            
         }
-        
-
+        //this.GetComponent<Renderer>().material.color = new Color(0,0,0, 1);
+        GetComponent<Renderer>().material.mainTexture = _defaultAlbedo;
     }
     private void FixedUpdate()
     {
@@ -56,38 +33,69 @@ public abstract class BlockController : MonoBehaviour
         {
             this.GetComponent<Renderer>().material.color += new Color(0, 0, 0, _fadeSpeed);
         }
+        else
+        {
+            _fadeSpeed = 0;
+        }
     }
     void BlockMaterialChange(ChangeType _fade)
     {
         if( _fade == ChangeType.FadeIn )
         {
-            _fadeSpeed = 0.1f;
-            if (this.GetComponent<Renderer>().material.color.a < 1)
+            if (this.GetComponent<Renderer>().material.color.a < 1 && this.GetComponent<Renderer>().material.color.a > 0)
             {
+                _fadeSpeed = 0.1f;
                 this.GetComponent<Renderer>().material.color += new Color(0, 0, 0, _fadeSpeed);
             }
         }
         else if( _fade == ChangeType.FadeOut )
         {
-            _fadeSpeed = -0.1f;
-            if (this.GetComponent<Renderer>().material.color.a > 0)
+            if (this.GetComponent<Renderer>().material.color.a > 0 && this.GetComponent<Renderer>().material.color.a < 1)
             {
+                _fadeSpeed = -0.1f;
                 this.GetComponent<Renderer>().material.color += new Color(0, 0, 0, _fadeSpeed);
             }
         }
-        else if( _fade == ChangeType.SwichIn )
+        else if( _fade == ChangeType.NonFadeIn )
         {
-            if (this.GetComponent<Renderer>().material.color.a == 1)
+            if (this.GetComponent<Renderer>().material.color.a < 1)
             {
-                this.GetComponent<Renderer>().material.color += new Color(0, 0, 0, -1);
+                this.GetComponent<Renderer>().material.color += new Color(0, 0, 0, 1);
             }
         }
         else
         {
-            if (this.GetComponent<Renderer>().material.color.a == 0)
+            if (this.GetComponent<Renderer>().material.color.a > 0)
             {
-                this.GetComponent<Renderer>().material.color += new Color(0, 0, 0, 1);
+                this.GetComponent<Renderer>().material.color += new Color(0, 0, 0, -1);
             }
+        }
+    }
+    void BlockControll()
+    {
+        if (_type == ObjectType.Block)
+        {
+            if (this.transform.position.x == GameController._intPlayer.x && this.transform.position.x <= GameController._intPlayer.x || this.transform.position.z == GameController._intPlayer.z && this.transform.position.x <= GameController._intPlayer.x)
+            {
+                BlockMaterialChange(ChangeType.NonFadeIn);
+            }
+            else if (this.transform.position.x > GameController._intPlayer.x)
+            {
+                BlockMaterialChange(ChangeType.FadeOut);
+            }
+            else
+            {
+                //this.GetComponent<Renderer>().material = _material[1];
+                //GetComponent<Renderer>().material.color = new Color(0, 0, 0, 0.5f);
+                //this.GetComponent<Renderer>().material.color += new Color(0, 0, 0, 0.5f);
+                BlockMaterialChange(ChangeType.NonFadeOut);
+
+            }
+
+        }
+        else if (_type == ObjectType.Sprite)
+        {
+
         }
     }
     enum ObjectType
@@ -104,8 +112,8 @@ public abstract class BlockController : MonoBehaviour
         /// <summary>フェードアウトで退場</summary>
         FadeOut,
         /// <summary>切り替えタイプで出現</summary>
-        SwichIn,
+        NonFadeIn,
         /// <summary>切り替えタイプで退場</summary>
-        SwichOut,
+        NonFadeOut,
     }
 }
