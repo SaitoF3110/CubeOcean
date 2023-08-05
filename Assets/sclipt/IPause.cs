e12db61faf4414669b4e18f3bb27821a074e4371
SaitoF3110 <water.tokotoko.drive@gmail.com>
2023-08-06 08:11:59 +0900

    ポーズとプレイヤーステータス追加

---
+++ Assets/sclipt/IPause.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IPause
+{
+    /// <summary>í‚é~</summary>
+    void Pause();
+    /// <summary>çƒê∂</summary>
+    void Resume();
+}
---
+++ Assets/sclipt/PlayerStates.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStates : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int _helth = 10;
+    int _maxHelth = 10;
+    int _speed = 1;
+    int _attack = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public enum DeBuff
+    {
+        None,
+        /// <summary>“Åó‘Ô</summary>
+        Poisoned,
+        /// <summary>–ƒáƒó‘Ô</summary>
+        Paralyzed,
+        /// <summary>“İ‰»ó‘Ô</summary>
+        Slowed,
+    }
+    public enum Buff
+    {
+        None,
+        /// <summary>ƒŠƒWƒFƒló‘Ô</summary>
+        Regenerating,
+        /// <summary>UŒ‚‹­‰»ó‘Ô</summary>
+        Strenth,
+        /// <summary>–hŒä‹­‰»ó‘Ô</summary>
+        Resistance,
+    }
+}
