48c3dac192a0a8d529e2adfc9d43ab2d4e024b4a
SaitoF3110 <water.tokotoko.drive@gmail.com>
2023-08-09 18:14:19 +0900

    データ追加

--- Assets/sclipt/Map/PanelController.cs
+++ Assets/sclipt/Map/PanelController.cs
@@ -17,7 +17,21 @@ public class PanelController : MonoBehaviour
     }
     void PlayerFactingPanel(FactingManager.Facting _fact, bool _turn)
     {
-        
+        GameObject _playerObj = GameObject.FindWithTag("Player");
+        float _fixdTrans;
+        //プレイヤーの位置修正
+        if (_fact == FactingManager.Facting.PlusZ || _fact == FactingManager.Facting.MinusZ)
+        {
+            float _fixValue;
+            if (this.transform.position.x >= 0) { _fixValue = 0.45f; } else { _fixValue = -0.45f; }
+            _fixdTrans = _playerObj.transform.position.x + _fixValue;
+        }
+        else
+        {
+            float _fixValue;
+            if (this.transform.position.z >= 0) { _fixValue = 0.45f; } else { _fixValue = -0.45f; }
+            _fixdTrans = _playerObj.transform.position.z + _fixValue;
+        }
     }
     // Update is called once per frame
     void ColorControll(GameObject _panel,bool _inOut)
@@ -26,6 +40,13 @@ public class PanelController : MonoBehaviour
             _panel.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
         else
             _panel.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 1);
+    }
+    void Fade(bool _in)
+    {
+        if(_in)
+        {
+
+        }
     }
 }
 
